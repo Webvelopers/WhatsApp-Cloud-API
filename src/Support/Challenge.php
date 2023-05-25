9b04498fb3b32ce0d9d8c9bfd88be86a412f7f97
Webvelopers <webmaster@webvelopers.net>
2023-05-25 15:26:37 -0500

    new lines

--- src/Support/Challenge.php
+++ src/Support/Challenge.php
@@ -4,6 +4,9 @@ namespace Webvelopers\WhatsAppCloudApi\Support;
 
 class Challenge
 {
+    /**
+     * Returns challenge string like Hub Challenge by Meta.
+     */
     static public function make(): string
     {
         return random_int(100000000, 999999999);
